1dcd9446b2301f06b2bd949846c46c0c91938f95
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-05-06 18:47:15 -0400

    Update ContainsDuplicate.java

--- p217/ContainsDuplicate.java
+++ p217/ContainsDuplicate.java
@@ -1,14 +1,5 @@
 /*
 217. Contains Duplicate
-Easy
-
-753
-
-688
-
-Add to List
-
-Share
 Given an array of integers, find if the array contains any duplicates.
 
 Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
